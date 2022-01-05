5cab5ef991ce74feae5b78fce90f36727d9a27aa
shakilmahammud <hshakil987@gmail.com>
2022-01-06 00:01:18 +0600

    up

--- mysite/first_app/views.py
+++ mysite/first_app/views.py
@@ -9,3 +9,11 @@ def index(req):
     return render(req,'index.html',{'text_1':'I am a text sent from view.py','ami':ami_list})
 def contact(req):
     return HttpResponse("<h1>HI It's Contact Page</h1>")
+def contact(req):
+    return HttpResponse("<h1>HI It's Contact Page</h1>")
+
+def contact(req):
+    return HttpResponse("<h1>HI It's Contact Page</h1>")
+
+def contact(req):
+    return HttpResponse("<h1>HI It's Contact Page</h1>")
