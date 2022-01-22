5598b145b611af70216425949428168caf0a6a80
shakilmahammud <hshakil987@gmail.com>
2022-01-22 21:05:55 +0600

    django form validator test

---
+++ mysite/first_app/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from first_app.models import Ablum,Ami,Tumi,Musician
+
+class Userform(forms.Form):
+    user_mail  = forms.EmailField()
+    user_email = forms.EmailField()
+    
+    def clean(self):
+        cleaned_data = super().clean()
+        user_mail = cleaned_data["user_mail"]
+        user_email = cleaned_data["user_email"]
+
+        if user_mail != user_email:
+            # Only do something if both fields are valid so far.
+            raise forms.ValidationError(
+                    "Filed not match"
+                )
--- mysite/first_app/views.py
+++ mysite/first_app/views.py
@@ -1,10 +1,22 @@
+from urllib import request
 from django.shortcuts import render
 from django.http import HttpResponse
 from first_app.models import Ami,Tumi
+from first_app.forms import Userform
 # Create your views here.
 def index(req):
     #Select * From Ami Order by First name
     ami_list = Ami.objects.order_by('first_name')
     return render(req,'index.html',{'text_1':'I am a text sent from view.py','ami':ami_list})
 def form(req):
-    return render(req,'form.html',)
+    nform =Userform()
+    diction ={'Form':nform}
+    if req.method == 'POST':
+        nform = Userform(req.POST)
+        diction.update({'Form':nform})
+        if nform.is_valid():
+            diction.update({'user_name':'field match'})
+            diction.update({'submit':'Yes'})
+   
+    return render(req,'form.html',context=diction)
+    
